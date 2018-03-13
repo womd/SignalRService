a4ae76cb568c1f76840df838ad667be53c76b5e7
womd <chk.mailbox@gmail.com>
2018-03-13 18:31:22 +0100

    some tests....

---
+++ SignalRService/SignalRService/ViewModels/OrderHostConfigurationViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRService.ViewModels
+{
+    public class OrderHostConfigurationViewModel
+    {
+        public string AppendToSelector { get; set; }
+        public string SinalRGroup { get; set; }
+    }
+}
