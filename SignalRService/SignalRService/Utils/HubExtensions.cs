bbf25a94f93752194ec75d752d22e0afcacf07bc
womd <chk.mailbox@gmail.com>
2018-04-24 09:29:20 +0200

    test

---
+++ SignalRService/SignalRService/Utils/HubExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRService.Utils
+{
+    public static class HubExtensions
+    {
+        public static string GetRefererUrl(this IRequest Request)
+        {
+            return Request.GetHttpContext().Request.ServerVariables["HTTP_REFERER"];
+        }
+
+        public static string GetClientIp(this IRequest Request)
+        {
+            var httpcontext = Request.GetHttpContext();
+            var forwardedFor = httpcontext.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+            var userIpAddress = String.IsNullOrWhiteSpace(forwardedFor) ?
+                httpcontext.Request.ServerVariables["REMOTE_ADDR"] : forwardedFor.Split(',').Select(s => s.Trim()).First();
+            return userIpAddress;
+        }
+
+    }
+}
